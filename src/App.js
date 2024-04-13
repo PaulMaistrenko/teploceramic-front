5c1d56ef3b1de03519e7e3fda165546e54e98bc4
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-04-13 23:00:22 +0200

    copy project files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Outlet } from 'react-router-dom';
+// import './App.scss';
 
-function App() {
+import { Header } from './components/Header';
+import { Menu } from './components/Menu';
+import { useState } from 'react';
+import { Footer } from './components/Footer';
+
+export const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-       Header
-      </header>
+    <div className="page__body">
+      <Header
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+      />
+      <Menu isMenuOpen={isMenuOpen} />
+      <main className="main-content">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { Root } from './Root';
+
+const container = document.getElementById('root');
+
+ReactDOM.render(
+  <Root />,
+  container,
 );
