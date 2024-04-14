24cae688ab37c5c9c1d6b044978e2d56fe2d70ec
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-04-14 23:08:37 +0200

    implement top dehydrators page

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,13 +1,35 @@
+import { Link } from 'react-router-dom';
 import { ProductItem } from '../ProductItem';
 
 export const ProductList = () => {
   return (
     <div className="all-products__page">
-      <div className="container">
-        <div className="all-products__top">
-          <div className="all-products__info"></div>
-          <div className="all-products__actions"></div>
+      <div className="all-products__top">
+        <div className="all-products__info">
+          <div className="page__path">
+            <Link to="/" className="home__link icon" />
+            <div className="slach icon">/</div>
+            <p className="current__page">Dehydrators</p>
+          </div>
+          <h6 className="page__title text-color--primaryP500">DEHYDRATORS</h6>
+          <div className="questions__block">
+            <div className="questions__item">
+              <div className="question__text large__text--regular">What is a dehydrator?</div>
+              <button className="questions__button icon"></button>
+            </div>
+            <div className="questions__item">
+              <div className="question__text large__text--regular">What types of dehydrators are there?</div>
+              <button className="questions__button icon"></button>
+            </div>
+          </div>
         </div>
+        <div className="container">
+          <div className="all-products__actions">
+            <button className="filters-open__button button main__text--medium">Filters</button>
+          </div>
+        </div>
+      </div>
+       <div className="container">
         <ul className="product__list">
           <ProductItem />
           <ProductItem />
